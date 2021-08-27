e3b1620a31de4e9e139317e75ddb6110446ab7e6
sjh4773 <jihun950202@naver.com>
2021-08-27 15:58:25 +0900

    오버로딩

---
+++ 객체지향/Oop_10.java
@@ -0,0 +1,36 @@
+
+public class Oop_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyMath3 mm = new MyMath3();
+		System.out.println("mm.add(3, 3) 결과: " + mm.add(3, 3));
+		System.out.println("mm.add(3L, 3L) 결과: " + mm.add(3L, 3L));
+		
+		int[] a = {100, 200, 300};
+		System.out.println("mm.add(a) 결과: " + mm.add(a));
+
+	}
+
+}
+
+class MyMath3 {
+	int add(int a, int b) {
+		System.out.print("int add(int a, int b) - ");
+		return a + b;
+	}
+	
+	long add(long a, long b) {
+		System.out.print("long add(int a, long b) - ");
+		return a + b;
+	}
+	
+	int add(int[] a) {                // 배열의 모든 요소의 합을 결과로 돌려준다.
+		System.out.print("int add(int[] a) - ");
+		int result = 0;
+		for(int i=0 ; i < a.length; i++)
+			result += a[i];
+		
+		return result;
+	}
+}
