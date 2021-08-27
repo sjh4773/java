824c59d0b8a766748cef185e417b8dfce1ca332b
sjh4773 <jihun950202@naver.com>
2021-08-27 16:54:20 +0900

    생성자, 기본 생성자

---
+++ 객체지향/Oop_11.java
@@ -0,0 +1,24 @@
+class Data_1 {
+	int value;
+//    Data_1() {} // 기본생성자를 추가
+}
+
+class Data_2 {
+	int value;
+	
+	Data_2(){}; // 기본 생성자
+	
+	Data_2(int x) {  // 매개변수가 있는 생성자.
+		value = x;
+	}
+}
+
+public class Oop_11 {
+
+	public static void main(String[] args) {
+		Data_1 d1 = new Data_1();
+		Data_2 d2 = new Data_2(); // 기본 생성자가 없을 경우 compile error 발생
+
+	}
+
+}
